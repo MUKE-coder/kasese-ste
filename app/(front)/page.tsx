e5e907ce7ec0138614381e54bfd34fa274a4346c
kooza-colline <koozacollinz1@gmail.com>
2025-01-09 19:22:37 +0300

    added the Department Component

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,9 +1,11 @@
+import DepartmentComponent from "@/components/frontend/departments-component";
 import MedicalServices from "@/components/frontend/medical-services";
 
 export default function Home() {
   return (
     <div>
       <MedicalServices/>
+      <DepartmentComponent/>
     </div>
   );
 }
