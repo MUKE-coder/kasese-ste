122849f57da46fe8eed27fda2dee0a3aa0a67428
MUKE JOHNBAPTIST <64189841+MUKE-coder@users.noreply.github.com>
2025-01-09 19:31:49 +0300

    merged

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,7 +1,6 @@
 import DepartmentComponent from "@/components/frontend/departments-component";
 import HeroSection from "@/components/frontend/hero";
 import MedicalServices from "@/components/frontend/medical-services";
-import Image from "next/image";
 
 export default function Home() {
   return (
